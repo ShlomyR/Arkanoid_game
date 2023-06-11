9f9259dcfd0f46a2553ed48a85c5e66cc8b02b9e
ShlomyR <rshlomy@gmail.com>
2023-06-11 12:56:36 +0300

    fixing not getting name

--- src/sourcefiles/Commands/InitObjectsCommand.cpp
+++ src/sourcefiles/Commands/InitObjectsCommand.cpp
@@ -6,7 +6,7 @@ void InitObjectsCommand::DoCommand(std::vector<std::string> const&line)
 {
     WhileCommand wc;
     sf::Vector2u windowSize = sf::Vector2u (std::min(800u, sf::VideoMode::getDesktopMode().width), std::min(600u, sf::VideoMode::getDesktopMode().height));
-    std::string gameTitle = line[1];
+    std::string gameTitle = wc.getStringVal(line[1]);
     int health = wc.getVal(line[2]);
     int level = wc.getVal(line[3]);
     int finaleLevel = wc.getVal(line[4]);
