8c12fccdc5b3e3fe2ded9b32c328348edf224a2e
ShlomyR <rshlomy@gmail.com>
2023-06-11 12:57:53 +0300

    cleaning code and adding stets

--- src/sourcefiles/InputLogic/WindowArrowsKey.cpp
+++ src/sourcefiles/InputLogic/WindowArrowsKey.cpp
@@ -8,11 +8,29 @@ WindowArrowsKey::WindowArrowsKey(sf::RenderWindow &window, GameState &gameState)
 
 }
 
+void up()
+{
+    // m_menu.up();
+}
+
+void down()
+{
+    // m_menu.down();
+}
+
 void WindowArrowsKey::escape()
 {
-    if (m_gameState.getState() != State::MenuPage) {
+    if (m_gameState.getState() == State::DifficultyPage) {
+        m_gameState.setState(State::OptionsPage);
+    } else if (m_gameState.getState() == State::OptionsPage) {
         m_gameState.setState(State::MenuPage);
-    } else {
-        m_window.close(); 
+    } else if (m_gameState.getState() == State::MenuPage) {
+        m_window.close();
+    } else if (m_gameState.getState() == State::ControlSettingPage) {
+        m_gameState.setState(State::OptionsPage);
+    } else if (m_gameState.getState() == State::VideoPage) {
+        m_gameState.setState(State::OptionsPage);
+    } else if (m_gameState.getState() != State::OptionsPage) {
+        m_gameState.setState(State::MenuPage);
     }
 }
