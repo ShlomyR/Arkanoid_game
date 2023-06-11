52d0619fda3475fc98ca96575d158665432edf56
ShlomyR <rshlomy@gmail.com>
2023-06-11 12:44:02 +0300

    adding methods to abstract class

--- src/hederfiles/Interfaces/MenuOption.hxx
+++ src/hederfiles/Interfaces/MenuOption.hxx
@@ -9,12 +9,14 @@ class MenuOption
 {
 public:
     virtual ~MenuOption() = default;
-    virtual void draw(sf::RenderWindow& window) = 0;
-    virtual bool handleInput(InputHandler& inputHandler,GameState &gameState) = 0;
+    virtual void draw(sf::RenderWindow& ) = 0;
+    virtual bool handleInput(InputHandler& ,GameState &) = 0;
     virtual void makeButton(sf::RenderWindow&) = 0;
     virtual bool isSelected() const = 0;
     virtual void setIsSelected(bool) = 0;
-    virtual sf::Text getText() = 0;
+    virtual void setIsTranslucent(bool value) = 0;
+    virtual sf::Text* getText() = 0;
 protected:
     sf::Font m_font;
+    int m_firstButtonPos;
 };
