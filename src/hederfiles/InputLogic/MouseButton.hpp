767c1310f4c6ee506b21985ae2b1a1cb2490124d
ShlomyR <rshlomy@gmail.com>
2023-06-11 12:26:12 +0300

    fixing bug-clicking on buttons not as needed

--- src/hederfiles/InputLogic/MouseButton.hpp
+++ src/hederfiles/InputLogic/MouseButton.hpp
@@ -4,15 +4,21 @@
 
 class GameState;
 class Menu;
+class Options;
+class Difficulty;
+class Video;
 
 class MouseButton
 {
 public:
-    MouseButton(sf::RenderWindow &, Menu &, GameState &);
+    MouseButton(sf::RenderWindow &, Menu &, Options &, Difficulty &, Video &, GameState &);
     ~MouseButton() = default;
     void left(bool const&);
 private:
     sf::RenderWindow &m_window;
     Menu &m_menu;
+    Options &m_options;
+    Difficulty &m_difficulty;
+    Video &m_video;
     GameState &m_gameState;
 };
--- src/sourcefiles/InputLogic/MouseButton.cpp
+++ src/sourcefiles/InputLogic/MouseButton.cpp
@@ -1,15 +1,35 @@
-#include "MouseButton.hpp"
 #include "InputHandlerImpl.hpp"
+#include "MouseButton.hpp"
+#include "Difficulty.hpp"
+#include "Video.hpp"
 #include "GameState.hpp"
+#include "Options.hpp"
 #include "Menu.hpp"
 
-MouseButton::MouseButton(sf::RenderWindow &window, Menu &menu, GameState &gameState)
+#include <iostream>
+
+MouseButton::MouseButton(sf::RenderWindow &window, Menu &menu, Options &options, Difficulty &difficulty, Video &video, GameState &gameState)
 : m_window(window)
 , m_menu(menu)
+, m_options(options)
+, m_difficulty(difficulty)
+, m_video(video)
 , m_gameState(gameState)
 {}
 
 void MouseButton::left(bool const& value)
 {
-    m_menu.setIsMenuShown(value);
+    if (m_menu.getIsMenuPageShown()) {
+        m_menu.setIsSelected();
+        m_menu.setIsMenuPageShown(value);
+    } else if (m_options.getIsOptionsPageShown()) {
+        m_options.setIsSelected();
+        m_options.setIsOptionsPageShown(value);
+    } else if (m_difficulty.getIsDifficultyPageShown()) {
+        m_difficulty.setIsSelected();
+        m_difficulty.setIsDifficultyPageShown(value);
+    } else if (m_video.getIsVideoPageShown()) {
+        m_video.setIsSelected();
+        m_video.setIsVideoPageShown(value);
+    }
 }
