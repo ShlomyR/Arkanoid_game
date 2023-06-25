5bfc1037588a7be5cf784c6aa66b395ba4d9f655
ShlomyR <rshlomy@gmail.com>
2023-06-25 23:04:58 +0300

    adding menuScreen and movie objects

--- src/sourcefiles/Renderer.cpp
+++ src/sourcefiles/Renderer.cpp
@@ -10,6 +10,8 @@
 #include "Video.hpp"
 #include "ControlSettingsMenu.hpp"
 #include "VolumeManager.hpp"
+#include "MenuScreenHandler.hpp"
+#include "VideoPlayer.hpp"
 
 RendererImpl::RendererImpl(WindowManager &window
     , GameState &gameState
@@ -25,6 +27,8 @@ RendererImpl::RendererImpl(WindowManager &window
     , Background &background
     , ControlSettingsMenu &controlSettingsMenu
     , VolumeManager &volume
+    , MenuScreenHandler &menuScreenHandler
+    , VideoPlayer &movie
 )
 : m_window(window)
 , m_gameState(gameState)
@@ -40,18 +44,28 @@ RendererImpl::RendererImpl(WindowManager &window
 , m_background(background)
 , m_controlSettingsMenu(controlSettingsMenu)
 , m_volume(volume)
+, m_menuScreenHandler(menuScreenHandler)
+, m_movie(movie)
 {
-    
+    std::string file = "The_Last_of_Us_Remastered_Main_Menu_at_4K_(All_Tracks).mp4";
+    m_movie.playVideo(file);
 }
 
 void RendererImpl::render()
 {
+    m_movie.update();
     if (m_gameState.getState() == State::MenuPage) {
+        if (!m_movie.isPlaying()) {
+            m_movie.resume();
+        }
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
         m_menu.drew();
         m_window.display();
     } else if (m_gameState.getState() == State::PlayPage) {
+        if (m_movie.isPlaying()) {
+            m_movie.pause();
+        }
         m_window.clear();
         m_background.draw(m_window.getRenderWindow());
         m_border.draw(m_window.getRenderWindow());
@@ -62,37 +76,51 @@ void RendererImpl::render()
         m_window.display();
     } else if (m_gameState.getState() == State::HighScorePage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_hud.draw(m_window.getRenderWindow(), "HighScorePage");
         m_window.display();
     } else if (m_gameState.getState() == State::UpdateHighScorePage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_hud.draw(m_window.getRenderWindow(), "UpdateHighScorePage");
         m_window.display();
     } else if (m_gameState.getState() == State::OptionsPage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_options.drew();
         m_window.display();
     } else if (m_gameState.getState() == State::DifficultyPage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_difficulty.drew();
         m_window.display();
     } else if (m_gameState.getState() == State::VideoPage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_video.drew();
         m_window.display();
     } else if (m_gameState.getState() == State::ControlSettingPage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_controlSettingsMenu.draw();
         m_window.display();
     } else if (m_gameState.getState() == State::VolumePage) {
         m_window.clear();
-        m_background.draw(m_window.getRenderWindow());
+        m_movie.render();
+        m_window.draw(m_movie.getBackground());
+        m_menuScreenHandler.draw();
         m_volume.draw();
         m_window.display();
     }
