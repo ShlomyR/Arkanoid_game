14d2cb23d53ead4ab15c982e73ac9210d45d106f
ShlomyR <rshlomy@gmail.com>
2023-06-25 23:07:31 +0300

    adding menuScreen for position

--- src/hederfiles/VolumeManager.hpp
+++ src/hederfiles/VolumeManager.hpp
@@ -3,17 +3,22 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
-class VolumeManager {
+class MenuScreenHandler;
+
+class VolumeManager 
+{
 public:
-    VolumeManager(sf::RenderWindow &);
+    VolumeManager(sf::RenderWindow &, MenuScreenHandler&);
 
     void setVolume(int);
     void increaseVolume(int);
     void decreaseVolume(int);
+    std::vector<std::pair<std::string, sf::Text *>> getTexts();
     void draw();
     void updateMouseMoved(sf::Vector2f&);
     void updateMousePrest(sf::Vector2f&);
     void updateText(const sf::Vector2u&);
+    int getVolume();
 private:
     void initializeGUI();
     void makeText(sf::Text &, sf::Font &, std::string, sf::Vector2i);
@@ -22,11 +27,12 @@ private:
     int m_volume;
     bool static m_isMusicPlayed;
     sf::Font m_font;
+    sf::Text m_masterVolumeText;
+    sf::Text m_muteText;
     sf::Text m_pluseText;
     sf::Text m_minusText;
     sf::Text m_volumeText;
-    sf::Text m_muteText;
     std::vector<std::pair<std::string, sf::Text *>> m_controlTexts;
     sf::RenderWindow &m_window;
-
+    MenuScreenHandler &m_menuScreenHandler;
 };
--- src/sourcefiles/VolumeManager.cpp
+++ src/sourcefiles/VolumeManager.cpp
@@ -1,35 +1,38 @@
 #include "VolumeManager.hpp"
 #include "SoundManager.hpp"
+#include "MenuScreenHandler.hpp"
 
 #include <iostream>
 
 bool VolumeManager::m_isMusicPlayed;
 
-VolumeManager::VolumeManager(sf::RenderWindow &window)
+VolumeManager::VolumeManager(sf::RenderWindow &window, MenuScreenHandler &menuScreenHandler)
 : m_volume(100)
 , m_window(window)
+, m_menuScreenHandler(menuScreenHandler)
 {
     initializeGUI();
 }
 
 void VolumeManager::initializeGUI()
 {
-    if (!m_font.loadFromFile("src/fonts/kenVectoFuture2.ttf")) {
+    if (!m_font.loadFromFile("src/fonts/DIN.ttf")) {
         std::cout << "Failed to load font!" << "\n";
     }
-
-    makeText(m_pluseText, m_font, "Pluse", sf::Vector2i(m_window.getSize().x - 700, 10));
-    makeText(m_minusText, m_font, "Minus", sf::Vector2i(m_window.getSize().x -700, 110));
-    makeText(m_volumeText, m_font, "Volume: " + std::to_string(m_volume) + " %", sf::Vector2i(m_window.getSize().x - 700, 210));
-    makeText(m_muteText, m_font, "Mute", sf::Vector2i(m_window.getSize().x - 210, 10));
+    makeText(m_masterVolumeText, m_font, "Master Volume", sf::Vector2i(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + 40, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100));
+    makeText(m_muteText, m_font, "Mute Volume", sf::Vector2i(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + 40, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 150));
+    makeText(m_minusText, m_font, "<", sf::Vector2i(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + m_menuScreenHandler.getOptionsBoxShape().getSize().x - 150, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100));
+    makeText(m_volumeText, m_font, std::to_string(m_volume) + " %", sf::Vector2i(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + m_menuScreenHandler.getOptionsBoxShape().getSize().x - 100, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100));
+    makeText(m_pluseText, m_font, ">", sf::Vector2i(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + m_menuScreenHandler.getOptionsBoxShape().getSize().x , m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100));
 }
 
 void VolumeManager::draw()
 {
+    m_window.draw(m_masterVolumeText);
+    m_window.draw(m_muteText);
     m_window.draw(m_pluseText);
     m_window.draw(m_minusText);
     m_window.draw(m_volumeText);
-    m_window.draw(m_muteText);
 }
 
 void VolumeManager::updateMouseMoved(sf::Vector2f& mousePosition)
@@ -38,13 +41,13 @@ void VolumeManager::updateMouseMoved(sf::Vector2f& mousePosition)
     for (auto& controlText : m_controlTexts) {
         if (controlText.second->getGlobalBounds().contains(mousePosition)) {
             isMouseHovering = true;
-            controlText.second->setOutlineColor(sf::Color::Blue);
-            controlText.second->setFillColor(sf::Color::Yellow);
+            controlText.second->setOutlineColor(sf::Color::White);
+            controlText.second->setFillColor(sf::Color::White);
             controlText.second->setOutlineThickness(2);
         } else {
-            controlText.second->setOutlineColor(sf::Color::White);
+            controlText.second->setOutlineColor(sf::Color{ 128,128,128 });
             controlText.second->setOutlineThickness(0);
-            controlText.second->setFillColor(sf::Color::White);
+            controlText.second->setFillColor(sf::Color{ 128,128,128 });
         }
     }
 
@@ -58,14 +61,13 @@ void VolumeManager::updateMouseMoved(sf::Vector2f& mousePosition)
 
 void VolumeManager::updateMousePrest(sf::Vector2f& mousePosition)
 {
-    // int currVolume = 0;
     for (auto& controlText : m_controlTexts) {
         if (controlText.second->getGlobalBounds().contains(mousePosition)) {
-            if (controlText.first == "Minus") {
+            if (controlText.first == "<") {
                 decreaseVolume(10);
-            } else if (controlText.first == "Pluse") {
+            } else if (controlText.first == ">") {
                 increaseVolume(10);
-            } else if (controlText.first == "Mute") {
+            } else if (controlText.first == "Mute Volume") {
                 if (m_volume != 0) {
                     setVolume(0);
                 } else {
@@ -77,35 +79,64 @@ void VolumeManager::updateMousePrest(sf::Vector2f& mousePosition)
     }
 }
 
+std::vector<std::pair<std::string, sf::Text *>> VolumeManager::getTexts()
+{
+    return m_controlTexts;
+}
+
 void VolumeManager::makeText(sf::Text &text, sf::Font &font, std::string str, sf::Vector2i vec2i)
 {
-    if (str != "Volume: " + std::to_string(m_volume) + " %") {
+    if (str != std::to_string(m_volume) + " %") {
         text.setFont(font);
         text.setCharacterSize(48);
-        text.setFillColor(sf::Color::White);
+        text.setFillColor(sf::Color{ 128,128,128 });
         text.setString(str);
+        text.setScale(0.5, 0.5);
         text.setPosition(vec2i.x,vec2i.y);
         m_controlTexts.emplace_back(str, &text);
     } else {
         text.setFont(font);
         text.setCharacterSize(48);
-        text.setFillColor(sf::Color::Green);
+        text.setFillColor(sf::Color::White);
         text.setString(str);
-        text.setOutlineColor(sf::Color::Blue);
+        text.setScale(0.5, 0.5);
+        text.setOutlineColor(sf::Color::White);
         text.setOutlineThickness(2);
         text.setPosition(vec2i.x,vec2i.y);
     }
 }
 
-void VolumeManager::updateText(const sf::Vector2u &windowSize)
+void VolumeManager::updateText(const sf::Vector2u &)
 {
-    int size = -200;
-    for (auto& controlText : m_controlTexts) {
-        sf::Vector2f buttonPos = sf::Vector2f(windowSize.x / 2 - controlText.second->getGlobalBounds().width / 2, windowSize.y / 2 + size);
-        controlText.second->setPosition(buttonPos);
-        size += 100;
-    }
-    m_volumeText.setPosition(sf::Vector2f(windowSize.x / 2 - m_volumeText.getGlobalBounds().width / 2, windowSize.y / 2 + size));
+    m_masterVolumeText.setPosition(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + 40, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100);
+    m_muteText.setPosition(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + 40, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 150);
+    m_minusText.setPosition(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + m_menuScreenHandler.getOptionsBoxShape().getSize().x - 150, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100);
+    m_volumeText.setPosition(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + m_menuScreenHandler.getOptionsBoxShape().getSize().x - 100, m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100);
+    m_pluseText.setPosition(m_menuScreenHandler.getOptionsBoxShape().getPosition().x + m_menuScreenHandler.getOptionsBoxShape().getSize().x , m_menuScreenHandler.getOptionsBoxShape().getPosition().y + 100);
+    // if (windowSize.x == 1024) {
+    //     m_masterVolumeText.setScale(0.4,0.4);
+    //     m_muteText.setScale(0.4,0.4);
+    //     m_minusText.setScale(0.4,0.4);
+    //     m_volumeText.setScale(0.4,0.4);
+    //     m_pluseText.setScale(0.4,0.4);
+    // } else if (windowSize.x == 800) {
+    //     m_masterVolumeText.setScale(0.3,0.3);
+    //     m_muteText.setScale(0.3,0.3);
+    //     m_minusText.setScale(0.3,0.3);
+    //     m_volumeText.setScale(0.3,0.3);
+    //     m_pluseText.setScale(0.3,0.3);
+    // } else if (windowSize.x == 1920) {
+    //     m_masterVolumeText.setScale(0.5,0.5);
+    //     m_muteText.setScale(0.5,0.5);
+    //     m_minusText.setScale(0.5,0.5);
+    //     m_volumeText.setScale(0.5,0.5);
+    //     m_pluseText.setScale(0.5,0.5);
+    // }
+}
+
+int VolumeManager::getVolume()
+{
+    return m_volume;
 }
 
 void VolumeManager::setVolume(int volume)
@@ -113,7 +144,7 @@ void VolumeManager::setVolume(int volume)
     if (volume >= 0 && volume <= 100) {
         m_volume = volume;
         updateVolume();
-        m_volumeText.setString("Volume: " + std::to_string(m_volume) + " %");
+        m_volumeText.setString(std::to_string(m_volume) + " %");
     }
 }
 
