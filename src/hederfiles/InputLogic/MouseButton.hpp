eb7400a03ad985e82357d3a9c0e4027936375f8e
ShlomyR <rshlomy@gmail.com>
2023-06-25 23:14:26 +0300

    adding menuScreen obj

--- src/hederfiles/InputLogic/MouseButton.hpp
+++ src/hederfiles/InputLogic/MouseButton.hpp
@@ -4,11 +4,12 @@ class Menu;
 class Options;
 class Difficulty;
 class Video;
+class MenuScreenHandler;
 
 class MouseButton
 {
 public:
-    MouseButton(Menu &, Options &, Difficulty &, Video &);
+    MouseButton(Menu &, Options &, Difficulty &, Video &, MenuScreenHandler &);
     ~MouseButton() = default;
     void left(bool const&);
 private:
@@ -16,4 +17,5 @@ private:
     Options &m_options;
     Difficulty &m_difficulty;
     Video &m_video;
+    MenuScreenHandler &m_menuScreenHandler;
 };
