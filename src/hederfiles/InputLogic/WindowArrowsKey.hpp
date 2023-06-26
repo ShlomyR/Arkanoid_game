67619c89ab23049834727d8b1509fb7db396834b
ShlomyR <rshlomy@gmail.com>
2023-06-26 11:43:50 +0300

    Revert "adding up , down, enter methods"
    This reverts commit 066cb8804e787e9d16f8f3c77bcd3a4cbace281b.

--- src/hederfiles/InputLogic/WindowArrowsKey.hpp
+++ src/hederfiles/InputLogic/WindowArrowsKey.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
-#include "MenuOption.hxx"
-
 #include <SFML/Graphics.hpp>
-#include <unordered_map>
-#include <memory>
 
 class GameState;
 
@@ -14,22 +10,8 @@ public:
     WindowArrowsKey(sf::RenderWindow &, GameState &);
     ~WindowArrowsKey() = default;
 
-    void up();
-    void down();
-    void enter();
     void escape();
 private:
     sf::RenderWindow &m_window;
     GameState &m_gameState;
-    std::string currPage;
-    int m_currMenu;
-    int m_maxIndexMenu;
-    std::vector<sf::Color> m_menuColors;
-    int m_currVideo;
-    int m_maxIndexVideo;
-    std::vector<sf::Color> m_videoColors;
-    int m_currOptions;
-    int m_maxIndexOptions;
-    std::vector<sf::Color> m_optionsColors;
-    std::unordered_map<std::string,std::vector<std::shared_ptr<MenuOption>>> m_map;
 };
--- src/sourcefiles/InputLogic/WindowArrowsKey.cpp
+++ src/sourcefiles/InputLogic/WindowArrowsKey.cpp
@@ -5,115 +5,17 @@ WindowArrowsKey::WindowArrowsKey(sf::RenderWindow &window, GameState &gameState)
 : m_window(window)
 , m_gameState(gameState)
 {
-    currPage = "MenuPage";
-    m_currMenu = 0;
-    m_currVideo = 0;
-    m_currOptions = 0;
-    m_map.insert({"VideoPage",m_gameState.getVideo().getTexts()});
-    m_map.insert({"MenuPage",m_gameState.getMenu().getTexts()});
-    m_map.insert({"DifficultyPage",m_gameState.getDifficulty().getTexts()});
-    m_map.insert({"OptionsPage",m_gameState.getOptions().getTexts()});
-    // m_map.insert({"ControlSettingPage",m_gameState.getOptions().getTexts()});
 
-    m_maxIndexMenu = 4;
-    m_menuColors = {
-        sf::Color::White,
-        sf::Color{128, 128, 128},
-        sf::Color{128, 128, 128},
-        sf::Color{128, 128, 128}
-    };
-    m_maxIndexVideo = 3;
-    m_videoColors = {
-        sf::Color::White,
-        sf::Color{128, 128, 128},
-        sf::Color{128, 128, 128}
-    };
-    m_maxIndexOptions = 4;
-    m_optionsColors = {
-        sf::Color::White,
-        sf::Color{128, 128, 128},
-        sf::Color{128, 128, 128},
-        sf::Color{128, 128, 128}
-    };
-    // m_map.insert({currPage,m_gameState.getMenu().getOption(2)});
-    // m_map.insert({currPage,m_gameState.getMenu().getOption(3)});
 }
 
-void WindowArrowsKey::up()
+void up()
 {
-    if (m_gameState.getState() == State::MenuPage) {
-        if (m_currMenu >= m_maxIndexMenu) {
-            m_currMenu = 0;
-        }
-
-        for (size_t i = 0; i < m_menuColors.size(); ++i) {
-            m_map.at("MenuPage").at(i)->getText()->setFillColor(m_menuColors[(m_currMenu + i) % m_maxIndexMenu]);
-        }
-
-        ++m_currMenu;
-    } else if (m_gameState.getState() == State::VideoPage) {
-        if (m_currVideo >= m_maxIndexVideo) {
-            m_currVideo = 0;
-        }
-
-        for (size_t i = 0; i < m_videoColors.size(); ++i) {
-            m_map.at("VideoPage").at(i)->getText()->setFillColor(m_videoColors[(m_currVideo + i) % m_maxIndexVideo]);
-        }
-
-        ++m_currVideo;
-    } else if (m_gameState.getState() == State::OptionsPage) {
-        if (m_currOptions >= m_maxIndexOptions) {
-            m_currOptions = 0;
-        }
-
-        for (size_t i = 0; i < m_optionsColors.size(); ++i) {
-            m_map.at("OptionsPage").at(i)->getText()->setFillColor(m_optionsColors[(m_currOptions + i) % m_maxIndexOptions]);
-        }
-
-        ++m_currOptions;
-    }
+    // m_menu.up();
 }
 
-void WindowArrowsKey::down()
+void down()
 {
-    
-    if (m_gameState.getState() == State::MenuPage) {
-        if (m_currMenu <= 0) {
-            m_currMenu = m_maxIndexMenu;
-        }    
-        for (size_t i = 0; i < m_menuColors.size(); ++i) {
-            m_map.at("MenuPage").at(i)->getText()->setFillColor(m_menuColors[(m_currMenu - m_maxIndexMenu + i - 1) % m_maxIndexMenu]);
-        }
-
-        --m_currMenu;
-
-    } else if (m_gameState.getState() == State::VideoPage) {
-        if (m_currVideo <= 0) {
-            m_currVideo = m_maxIndexVideo;
-        }
-
-        for (size_t i = 0; i < m_videoColors.size(); ++i) {
-            m_map.at("VideoPage").at(i)->getText()->setFillColor(m_videoColors[(m_currVideo - m_maxIndexVideo + i - 1) % m_maxIndexVideo]);
-        }
-
-        --m_currVideo;
-        
-    } else if (m_gameState.getState() == State::VolumePage) {
-        m_map.at("VolumePage").at(1)->getText()->setFillColor(sf::Color::Blue);
-    } else if (m_gameState.getState() == State::OptionsPage) {
-        if (m_currOptions <= 0) {
-            m_currOptions = m_maxIndexOptions;
-        }
-        for (size_t i = 0; i < m_optionsColors.size(); ++i) {
-            m_map.at("OptionsPage").at(i)->getText()->setFillColor(m_optionsColors[(m_currOptions - m_maxIndexOptions + i -1) % m_maxIndexOptions]);
-        }
-        --m_currOptions;
-    }
-}
-
-void WindowArrowsKey::enter()
-{
-
+    // m_menu.down();
 }
 
 void WindowArrowsKey::escape()
@@ -125,8 +27,6 @@ void WindowArrowsKey::escape()
     } else if (m_gameState.getState() == State::MenuPage) {
         m_window.close();
     } else if (m_gameState.getState() == State::ControlSettingPage) {
-        m_gameState.setState(State::OptionsPage);
-    } else if (m_gameState.getState() == State::VolumePage) {
         m_gameState.setState(State::OptionsPage);
     } else if (m_gameState.getState() == State::VideoPage) {
         m_gameState.setState(State::OptionsPage);
