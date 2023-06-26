36c94b910a23ce2e1fc200e42c3c8e5611e2e0c2
ShlomyR <rshlomy@gmail.com>
2023-06-26 12:30:15 +0300

    adding menuScreen obj

--- src/sourcefiles/InputLogic/MouseButton.cpp
+++ src/sourcefiles/InputLogic/MouseButton.cpp
@@ -1,3 +1,4 @@
+#include "MenuScreenHandler.hpp"
 #include "InputHandlerImpl.hpp"
 #include "MouseButton.hpp"
 #include "Difficulty.hpp"
@@ -7,11 +8,12 @@
 
 #include <iostream>
 
-MouseButton::MouseButton(Menu &menu, Options &options, Difficulty &difficulty, Video &video)
+MouseButton::MouseButton(Menu &menu, Options &options, Difficulty &difficulty, Video &video, MenuScreenHandler &menuScreenHandler)
 : m_menu(menu)
 , m_options(options)
 , m_difficulty(difficulty)
 , m_video(video)
+, m_menuScreenHandler(menuScreenHandler)
 {}
 
 void MouseButton::left(bool const& value)
@@ -20,12 +22,15 @@ void MouseButton::left(bool const& value)
         m_menu.setIsSelected();
         m_menu.setIsMenuPageShown(value);
     } else if (m_options.getIsOptionsPageShown()) {
+        m_menuScreenHandler.setIsSelected(true);
         m_options.setIsSelected();
         m_options.setIsOptionsPageShown(value);
     } else if (m_difficulty.getIsDifficultyPageShown()) {
+        m_menuScreenHandler.setIsSelected(true);
         m_difficulty.setIsSelected();
         m_difficulty.setIsDifficultyPageShown(value);
     } else if (m_video.getIsVideoPageShown()) {
+        m_menuScreenHandler.setIsSelected(true);
         m_video.setIsSelected();
         m_video.setIsVideoPageShown(value);
     }
