65a28af97c22de0d703b61611f02f9dc66ed2f33
ShlomyR <rshlomy@gmail.com>
2023-06-26 11:43:48 +0300

    Revert "updating map string words for the keys"
    This reverts commit 84e03b540a9b12fc88db3d222378b8379301db87.

--- src/sourcefiles/GameState.cpp
+++ src/sourcefiles/GameState.cpp
@@ -146,17 +146,17 @@ Paddle& GameState::getPaddle()
 
 sf::Keyboard::Key GameState::getBallRelease() const
 {
-    return m_controlSettings.getMapping("Release Ball");
+    return m_controlSettings.getMapping("Space");
 }
 
 sf::Keyboard::Key GameState::getLeft() const
 {
-    return m_controlSettings.getMapping("Move Paddle Left");
+    return m_controlSettings.getMapping("Left");
 }
 
 sf::Keyboard::Key GameState::getRight() const
 {
-    return m_controlSettings.getMapping("Move Paddle Right");
+    return m_controlSettings.getMapping("Right");
 }
 
 // void GameState::setControls(const std::string &str, sf::Keyboard::Key key)
