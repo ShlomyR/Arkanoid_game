71972f9ce4e002fcc7df49a513e526131d5b11d5
ShlomyR <rshlomy@gmail.com>
2023-06-26 12:30:44 +0300

    adding up, down, enter methods

--- src/hederfiles/InputLogic/WindowArrowsKey.hpp
+++ src/hederfiles/InputLogic/WindowArrowsKey.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "MenuOption.hxx"
+
 #include <SFML/Graphics.hpp>
+#include <unordered_map>
+#include <memory>
 
 class GameState;
 
@@ -10,8 +14,22 @@ public:
     WindowArrowsKey(sf::RenderWindow &, GameState &);
     ~WindowArrowsKey() = default;
 
+    void up();
+    void down();
+    void enter();
     void escape();
 private:
     sf::RenderWindow &m_window;
     GameState &m_gameState;
+    std::string currPage;
+    int m_currMenu;
+    int m_maxIndexMenu;
+    std::vector<sf::Color> m_menuColors;
+    int m_currVideo;
+    int m_maxIndexVideo;
+    std::vector<sf::Color> m_videoColors;
+    int m_currOptions;
+    int m_maxIndexOptions;
+    std::vector<sf::Color> m_optionsColors;
+    std::unordered_map<std::string,std::vector<std::shared_ptr<MenuOption>>> m_map;
 };
