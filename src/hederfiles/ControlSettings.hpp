41c4359f6d72acc912730ab683e43d98c7e3515b
ShlomyR <rshlomy@gmail.com>
2023-06-26 11:44:08 +0300

    Revert "adding ps4 map"
    This reverts commit 77f3d0930c208893347d8af284949b3b365513d2.

--- src/hederfiles/ControlSettings.hpp
+++ src/hederfiles/ControlSettings.hpp
@@ -2,7 +2,6 @@
 
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
-#include <variant>
 
 class ControlSettings
 {
@@ -11,11 +10,8 @@ public:
 
     void updateMapping(const std::string& action, sf::Keyboard::Key key);
     sf::Keyboard::Key getMapping(const std::string& action);
-    sf::Joystick::Axis getPs4Mapping(const std::string&);
     void defaultMappings();
 
 private:
     std::unordered_map<std::string, sf::Keyboard::Key> m_controlMappings;
-    std::unordered_map<std::string, sf::Joystick::Axis> m_controlMappingsPs4;
-    std::unordered_map<std::string, int> m_controlMappingsXbox;
 };
