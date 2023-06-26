422f1dc3b8d8dd55fff097fb112f0007aaf9d91d
ShlomyR <rshlomy@gmail.com>
2023-06-26 11:43:51 +0300

    Revert "adding menuScreen obj"
    This reverts commit 9f6c4b6eb8f9145bc19a28b1752ce6eb4700ccb2.

--- src/hederfiles/InputLogic/MouseButton.hpp
+++ src/hederfiles/InputLogic/MouseButton.hpp
@@ -4,12 +4,11 @@ class Menu;
 class Options;
 class Difficulty;
 class Video;
-class MenuScreenHandler;
 
 class MouseButton
 {
 public:
-    MouseButton(Menu &, Options &, Difficulty &, Video &, MenuScreenHandler &);
+    MouseButton(Menu &, Options &, Difficulty &, Video &);
     ~MouseButton() = default;
     void left(bool const&);
 private:
@@ -17,5 +16,4 @@ private:
     Options &m_options;
     Difficulty &m_difficulty;
     Video &m_video;
-    MenuScreenHandler &m_menuScreenHandler;
 };
--- src/sourcefiles/InputLogic/MouseButton.cpp
+++ src/sourcefiles/InputLogic/MouseButton.cpp
@@ -1,4 +1,3 @@
-#include "MenuScreenHandler.hpp"
 #include "InputHandlerImpl.hpp"
 #include "MouseButton.hpp"
 #include "Difficulty.hpp"
@@ -8,12 +7,11 @@
 
 #include <iostream>
 
-MouseButton::MouseButton(Menu &menu, Options &options, Difficulty &difficulty, Video &video, MenuScreenHandler &menuScreenHandler)
+MouseButton::MouseButton(Menu &menu, Options &options, Difficulty &difficulty, Video &video)
 : m_menu(menu)
 , m_options(options)
 , m_difficulty(difficulty)
 , m_video(video)
-, m_menuScreenHandler(menuScreenHandler)
 {}
 
 void MouseButton::left(bool const& value)
@@ -22,15 +20,12 @@ void MouseButton::left(bool const& value)
         m_menu.setIsSelected();
         m_menu.setIsMenuPageShown(value);
     } else if (m_options.getIsOptionsPageShown()) {
-        m_menuScreenHandler.setIsSelected(true);
         m_options.setIsSelected();
         m_options.setIsOptionsPageShown(value);
     } else if (m_difficulty.getIsDifficultyPageShown()) {
-        m_menuScreenHandler.setIsSelected(true);
         m_difficulty.setIsSelected();
         m_difficulty.setIsDifficultyPageShown(value);
     } else if (m_video.getIsVideoPageShown()) {
-        m_menuScreenHandler.setIsSelected(true);
         m_video.setIsSelected();
         m_video.setIsVideoPageShown(value);
     }
